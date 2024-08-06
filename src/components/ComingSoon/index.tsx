76cb5b6bf34c4c265fafbfde45caffd343029de5
Laurajcb <callejaslaura96@gmail.com>
2024-08-06 14:07:29 -0500

    [ADD] ComingSoon component with its styles

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -1,66 +1,81 @@
 import Image from "next/image";
+import PictureGroup from '../../../public/assets/imgs/groupPicture.jpg'
+import { TiStarFullOutline } from "react-icons/ti";
+import { TiGlobe } from "react-icons/ti";
+import { FaCheckCircle } from "react-icons/fa";
 
+
+import { FaLeaf } from "react-icons/fa";
 
 function ComingSoon() {
   return (
-    <section className="faq-section sec-pad">
-      <figure className="image-layer">
-        <Image src="/assets/images/banner/SliderAlfombras.png" alt="" width={100} height={100} />
-      </figure>
-      <div className="auto-container">
-        <div className="row clearfix">
-          <div className="col-lg-6 col-md-12 col-sm-12 content-column">
-            <div className="content_block_13">
-              <div className="content-box wow fadeInLeft animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                <div className="upper-box">
-                  <h3>Accreditations & Awards</h3>
-                  <p>Proud to share some of our achivements</p>
-                </div>
-                <div className="inner-box">
-                  <div className="single-box">
-                    <figure className="image-box">
-                      <Image src="/assets/images/resource/award-4.png" alt="" layout="fill" objectFit="contain" />
-                    </figure>
-                    <h4>ISO Accredited - 2005</h4>
-                    <p>Indignation  dislike men who are so demoralized by the charms of pleasure of the moment.</p>
-                  </div>
-                  <div className="single-box">
-                    <figure className="image-box">
-                      <Image src="/assets/images/resource/award-5.png" alt="" layout="fill" objectFit="contain" />
-                    </figure>
-                    <h4>Best Choice Award - 2008</h4>
-                    <p>Dislike men who are so demoralized the charms of pleasure of the moment.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-12 col-sm-12 content-column">
-            <div className="content_block_14">
-              <div className="content-box">
-                <div className="sec-title text-left">
-                  <h6>Coming soon</h6>
-                  <h2>Grandes Proyectos</h2>
-                </div>
-                <div>
-                  <p>
-                    Estamos liderando una innovadora iniciativa de limpieza y desinfección no tóxica bajo la dirección de Jeams Quiroz. Nuestro objetivo es ampliar nuestros servicios para incluir escuelas, hospitales, centros médicos y edificios residenciales de alta gama.
+    <section className="text_container_bigProyects">
+      <h6 className="title_custom ">Grandes Proyectos</h6>
 
-                    Hemos desarrollado un sistema de limpieza altamente eficiente, diseñado especialmente para entornos de salud, que prioriza la higiene y la seguridad sin el uso de productos químicos nocivos. Este sistema protege tanto a pacientes como a profesionales de la salud.
-
-                    Como proveedores de servicios de limpieza y sanitización, nos comprometemos a mantenernos a la vanguardia de la tecnología para reducir la contaminación ambiental y ofrecer la mejor calidad en nuestros servicios.
-
-
-
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      <section className="section_content">
+        <div className="section_content_imagen">
+          <Image 
+          alt="photo" 
+          src={PictureGroup} 
+          className="section_content_img"
+          />
         </div>
-      </div>
+        <div className="section_content_text">
+        <article className="article_bigProyects">
+          <div className="article_title">
+            <TiGlobe className="icon" />
+            <h4>Expansión de Servicios</h4>
+          </div>
+          <p>Nuestro proyecto principal es ampliar nuestro portafolio para brindar servicios de limpieza y
+            desinfección en escuelas, hospitales, centros médicos y edificios residenciales de alta gama.
+          </p>
+        </article>
+
+        <article className="article_bigProyects">
+          <div className="article_title">
+            <TiStarFullOutline className="icon" />
+            <h4>Innovación en Limpieza</h4>
+          </div>
+          <p>
+            Jeams Quiroz está liderando una iniciativa innovadora para desarrollar un sistema
+            de limpieza no tóxico y altamente eficiente.
+          </p>
+        </article>
+
+        <article className="article_bigProyects">
+          <div className="article_title">
+            <FaLeaf className="icon" />
+            <h4>Seguridad y Bienestar</h4>
+          </div>
+          <p>
+            Este sistema no solo prioriza la higiene y desinfección, sino que también garantiza
+            la seguridad y bienestar de los pacientes y profesionales de la salud al eliminar
+            el uso de productos químicos nocivos.
+          </p>
+        </article>
+
+        <article className="article_bigProyects">
+          <div className="article_title">
+            <FaCheckCircle className="icon" />
+            <h4>Calidad y Actualización</h4>
+          </div>
+          <p>
+            Como proveedores de limpieza y sanitización de espacios, tenemos presente que hay
+            un amplio territorio que necesita de nuestros servicios. Queremos seguir brindando
+            la mejor calidad y tecnología disponible y estar siempre actualizados para ayudar
+            a reducir la contaminación ambiental.
+          </p>
+        </article>
+
+        </div>
+       
+      </section>
+
     </section>
   )
 }
 
 export default ComingSoon;
+
+
+
